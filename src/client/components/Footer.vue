63b11d7e49002ad9d0adc1f6a4939f81aaa81a93
kiqiou <kuksare@gmail.com>
2026-05-29 17:54:17 +0300

    fix: fixed position for button for maps' links

--- src/client/components/Footer.vue
+++ src/client/components/Footer.vue
@@ -62,7 +62,7 @@
             class="w-full"
           ></iframe>
         </div>
-        <div class="mt-4 flex flex-col sm:flex-row gap-3 justify-center sm:justify-start">
+        <div class="mt-4 flex flex-col sm:flex-row gap-3 justify-around sm:justify-center">
           <a
             href="https://yandex.by/maps/?rtext=~Кальварийская%2021%20Минск"
             target="_blank"
@@ -70,7 +70,8 @@
             class="inline-flex items-center justify-center gap-2 px-6 py-3 bg-[#E5A663]/ border-1 border-black/70 text-black/70 font-bold rounded-xl hover:shadow-lg hover:scale-[1.02] transition-all duration-300"
           >
             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 7m0 13V7"></path>
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
             </svg>
             Построить маршрут в Яндекс
           </a>
